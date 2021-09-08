a67ec316b4a8d3055b89807556622686d397f5df
jjk1222 <totoro9789@gmail.com>
2021-09-08 15:58:17 +0900

    수정

--- asset/js/uidesign_1.js
+++ asset/js/uidesign_1.js
@@ -1,73 +1,28 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    //Navigation
-    menu: '#menu',
-    lockAnchors: false,
-    anchors: ['main','page1', '', '', '', '', ''],
+    //options here
+    autoScrolling:true,
+    // scrollHorizontally: true,
+    easing: 'easeInOutCubic',
     navigation: true,
-    navigationPosition: 'right',
-    navigationTooltips: ['','', '', '', '', '', ''],
+    navigationPosition: 'left',
+    navigationTooltips: ['box1','box2','box3','box4'],
     showActiveTooltip: true,
     slidesNavigation: true,
     slidesNavPosition: 'bottom',
-
-
-    //Scrolling
-    css3: true,
-    scrollingSpeed: 800,
-    autoScrolling: true,
-    fitToSection: true,
-    fitToSectionDelay: 1000,
-    scrollBar: false,
-    easing: 'easeInOutCubic',
-    easingcss3: 'ease',
-    loopBottom: false,
-    loopTop: false,
-    loopHorizontal: true,
-    continuousVertical: false,
-    continuousHorizontal: false,
-    scrollHorizontally: false,
-    interlockedSlides: false,
-    resetSliders: false,
-    fadingEffect: false,
-    normalScrollElements: '#element1, .element2',
     scrollOverflow: true,
     scrollOverflowOptions: null,
-    touchSensitivity: 15,
-    normalScrollElementTouchThreshold: 5,
-    bigSectionsDestination: null,
-
-    //Accessibility
-    keyboardScrolling: true,
-    animateAnchor: true,
-    recordHistory: true,
-
-    //Design
-    controlArrows: true,
-    verticalCentered: true,
-    sectionsColor : ['#f7fcfc', '#f7fcfc','#f7fcfc', '#f7fcfc','#f7fcfc', '#f7fcfc', '#f7fcfc'],
-    paddingTop: '0em',
-    paddingBottom: '0em',
-    fixedElements: '#header, .footer',
-    responsiveWidth: 0,
-    responsiveHeight: 0,
-    responsiveSlides: false,
-
-    //Custom selectors
-    sectionSelector: '.section',
-    slideSelector: '.slide',
+    menu: '#menu',
+    anchors: ['section1','section2', 'section3', 'section4'],
   });
 
   //methods
   $.fn.fullpage.setAllowScrolling(true);
 
 
-// mobile toggle gnb
-$("#toggle").click(function() {
-	$(this).toggleClass("open");
-	$("#gnb").toggleClass("opened");
-});
-
-
-
+  // mobile toggle gnb
+  $("#toggle").click(function() {
+    $(this).toggleClass("open");
+    $("#gnb").toggleClass("opened");
+  });
 });
